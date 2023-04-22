dbfb29ba787b7fa81a380ca5dc9700f1ded7cd97
RomainC <bebert64@gmail.com>
2023-04-22 14:37:13 +0200

    random wallpapers for theme3

--- dotfiles/conky/theme3/launch.sh
+++ dotfiles/conky/theme3/launch.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+WALLPAPER_NUM=$(shuf -i 1-4 -n 1)
 
 echo "launching"
-pkill conky
-feh --no-fehbg --bg-fill $SCRIPT_DIR/conky_bg.jpg
+pkill conky  
+feh --no-fehbg --bg-fill $SCRIPT_DIR/conky_bg$WALLPAPER_NUM.jpg
 conky -c $SCRIPT_DIR/qclocktwo -d
 echo "clock done"
 conky -c $SCRIPT_DIR/Zavijava-v1.6/Zavijava/Zavijava.conf ./ -d
