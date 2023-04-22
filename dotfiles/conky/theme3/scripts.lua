244cd14eea470bee46f024908cc791e0bf3b2b8a
RomainC <bebert64@gmail.com>
2023-04-22 14:38:09 +0200

    fix artist and album for conky theme3 change color for theme3

--- dotfiles/conky/theme3/scripts.lua
+++ dotfiles/conky/theme3/scripts.lua
@@ -5,3 +5,29 @@ function conky_myimg()
     local s = "${image "..path.." -s 256x256 -p 85x0}";
     return s;
 end
+
+function conky_artist()
+    local handle = io.popen([==[
+ARTIST="$(playerctl -p strawberry  metadata artist 2>&1)"
+if [[ "$ARTIST" == *"No player could handle this command"* || "$ARTIST" == *"No players found"* ]];then
+      ARTIST="";
+fi;
+echo $ARTIST
+]==])
+    local artist = handle:read("*a")
+    handle:close()
+    return ""..artist.."";
+end
+
+function conky_title()
+    local handle = io.popen([==[
+TITLE="$(playerctl -p strawberry metadata title 2>&1)"
+if [[ "$TITLE" == *"No player could handle this command"* || "$TITLE" == *"No players found"* ]];then
+        TITLE="";
+fi;
+echo $TITLE
+]==])
+    local title = handle:read("*a")
+    handle:close()
+    return ""..title.."";
+end
