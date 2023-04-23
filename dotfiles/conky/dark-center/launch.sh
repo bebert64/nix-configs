4a206354b12d235ba6d142f77fea22ae1ec8c216
RomainC <bebert64@gmail.com>
2023-04-23 10:05:26 +0200

    fix path typo

--- dotfiles/conky/dark-center/launch.sh
+++ dotfiles/conky/dark-center/launch.sh
@@ -6,6 +6,6 @@ WALLPAPER=$(ls $SCRIPT_DIR/wallpapers | sort -R | tail -1)
 echo  $SCRIPT_DIR/wallpapers/$WALLPAPER
 
 pkill conky  
-feh --no-fehbg --bg-fill $SCRIPT_DIR/wallpapers/$WALLPAPER
+feh --no-fehbg --bg-fill "$SCRIPT_DIR/wallpapers/$WALLPAPER"
 conky -c $SCRIPT_DIR/qclocktwo -d
 conky -c $SCRIPT_DIR/Zavijava-v1.6/Zavijava/Zavijava.conf ./ -d
