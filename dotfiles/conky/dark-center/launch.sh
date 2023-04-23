e6665cc3eb6142d150c8842e13b3a0f2b8f9775d
RomainC <bebert64@gmail.com>
2023-04-23 09:53:38 +0200

    fix missing file

---
+++ dotfiles/conky/dark-center/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+WALLPAPER=$(ls $SCRIPT_DIR/wallpapers | sort -R | tail -1)
+
+echo  $SCRIPT_DIR/wallpapers/$WALLPAPER
+
+pkill conky  
+feh --no-fehbg --bg-fill $SCRIPT_DIR/wallpapers/$WALLPAPER
+conky -c $SCRIPT_DIR/qclocktwo -d
+conky -c $SCRIPT_DIR/Zavijava-v1.6/Zavijava/Zavijava.conf ./ -d
