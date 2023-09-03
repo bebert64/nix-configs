3ea3e7f7088ca4c4ec0471be9eb890c77edf04db
DonBeberto <bebert64@gmail.com>
2023-09-03 15:33:42 +0100

    cleanup zsh config

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -8,13 +8,8 @@
         "upgrade" = "cd ~/nix-configs/home-manager && git pull && nix flake update --commit-lock-file && home-manager switch --flake .#${config_name} && git push";
         "c" = "code .";
         "r" = "ranger --choosedir=$HOME/.rangerdir; cd \"$(cat $HOME/.rangerdir)\"; rm $HOME/.rangerdir";
-        "datagrip-tunnel" = "ssh -L 5432:localhost:5432 charybdis";
         "wke1" = "i3-msg workspace \"\\\" \\\"\"";
         "mount-NAS" = "mount nas.capucina.house:/volume1/NAS";
-        "mount-fixe-bureau" = "sshfs fixe-bureau:/home/romain $HOME/Mnt/Cluster/fixe-bureau";
-        "mount-fixe-salon" = "sshfs fixe-salon:/home/romain $HOME/Mnt/Cluster/fixe-salon";
-        "mount-stockly-romainc" = "sshfs stockly-romainc:/home/romain $HOME/Mnt/Cluster/stockly-romainc";
-        "mount-raspy" = "sshfs raspy:/home/DonBeberto $HOME/Mnt/Cluster/raspy";
         "mount-Stockly" = "sshfs charybdis:/home/romain/Stockly/Main $HOME/Mnt/Charybdis";
     };
     history = {
