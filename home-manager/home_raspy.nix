42b39cd96c4b4cec645613ddb3a10953c6de1ee5
DonBeberto <bebert64@gmail.com>
2023-09-03 14:04:49 +0100

    test a few things for home-manager flakes

---
+++ home-manager/home_raspy.nix
@@ -0,0 +1,27 @@
+{ homeDirectory
+, pkgs
+, stateVersion
+, system
+, username }:
+
+{
+  home = {
+        homeDirectory = "/home/DonBeberto";
+        username = "DonBeberto"; # $USER
+        stateVersion = "23.05";     # See https://nixos.org/manual/nixpkgs/stable for most recent
+  };
+
+  nixpkgs = {
+    config = {
+        system = "aarch64-linux";  # x86_64-linux, aarch64-multiplatform, etc.
+      allowUnfree = true;
+      # allowUnsupportedSystem = true;
+      # experimental-features = "nix-command flakes";
+    };
+  };
+
+  programs = {
+    # zsh = import ./programs/zsh.nix;
+    home-manager.enable = true;
+  };
+}
