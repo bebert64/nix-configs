624d517da8d3ca35c45e83c9e7f02a869c492a11
DonBeberto <bebert64@gmail.com>
2023-09-03 16:31:55 +0100

    update ranger configuration

--- home-manager/home_raspy.nix
+++ home-manager/home_raspy.nix
@@ -2,17 +2,33 @@
 
 {
   home = {
-        homeDirectory = "/home/DonBeberto";
-        username = "DonBeberto"; # $USER
-        stateVersion = "23.05";     # See https://nixos.org/manual/nixpkgs/stable for most recent
+    homeDirectory = "/home/DonBeberto";
+    username = "DonBeberto";
+    stateVersion = "23.05";
+  };
+
+  home.packages = with pkgs;
+  [
+    ranger
+  ];
+
+  home.file = {
+    ".config/ranger/rc.conf".source = ../dotfiles/ranger/rc.conf;
+    ".config/ranger/scope.sh".source = ../dotfiles/ranger/scope.sh;
+  };
+  
+  # Activation script
+  home.activation = {
+    rangerBookmarks = ''
+      sed "s/\$USER/"$USER"/"  $HOME/nix-configs/dotfiles/ranger/bookmarks > $HOME/nix-configs/ranger_bookmarks
+      ln -sf $HOME/nix-configs/ranger_bookmarks $HOME/.local/share/ranger/bookmarks
+    '';
   };
 
   nixpkgs = {
     config = {
-        system = "aarch64-linux";  # x86_64-linux, aarch64-multiplatform, etc.
+      system = "aarch64-linux";  # x86_64-linux, aarch64-multiplatform, etc.
       allowUnfree = true;
-      # allowUnsupportedSystem = true;
-      # experimental-features = "nix-command flakes";
     };
   };
 
