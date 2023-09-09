80a677c09d91057bcc25cad777f8211ae058ca66
RomainC <bebert64@gmail.com>
2023-09-09 09:50:52 +0200

    set key map at i3 startup

--- home-manager/programs/i3.nix
+++ home-manager/programs/i3.nix
@@ -74,6 +74,7 @@
     startup = [
       { command = "mount -a"; }
       { command = "feh --bg-max --random ~/Wallpapers/Single\\ screen"; }
+      { command = "setxkbmap fr"; }
       # https://wiki.archlinux.org/title/GNOME/Keyring#Launching_gnome-keyring-daemon_outside_desktop_environments_(KDE,_GNOME,_XFCE,_...)
       { command = "dbus-update-activation-environment DISPLAY XAUTHORITY WAYLAND_DISPLAY"; notification = false; }
 
