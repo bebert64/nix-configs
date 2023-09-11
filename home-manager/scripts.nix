88920f571cb9dabed7bdd5ca01ee153c39d9bf49
RomainC <bebert64@gmail.com>
2023-09-11 18:43:02 +0200

    sshr

--- home-manager/scripts.nix
+++ home-manager/scripts.nix
@@ -7,6 +7,12 @@ host-specifics: { pkgs, ...}:
       nix-shell -p "$1" --command "''${1##*.} ''${*:2}"
     '')
 
+    (pkgs.writeScriptBin "sshr" ''
+      #!/usr/bin/env bash
+      set -euxo pipefail
+      tilix -p Ranger -e "ssh ''${1##*.}"
+    '')
+
     (pkgs.writeScriptBin "launch-rofi" ''
       #!/usr/bin/env bash
       
