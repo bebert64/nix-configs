ce096650b5d517ea4cc590f3289ff2dd3ef19439
RomainC <bebert64@gmail.com>
2023-09-12 09:06:40 +0200

    Revert "use nixos bootloader (faster builds)"
    This reverts commit 5a2ffde7ce2dfba30d317d830ce042052a982e9c.

--- nixos/fixe-bureau/configuration.nix
+++ nixos/fixe-bureau/configuration.nix
@@ -11,18 +11,14 @@
       ../software-configuration.nix
     ];
 
-  # # Use the grub EFI boot loader.
-  # boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.version = 2;
-  # boot.loader.grub.device = "nodev";
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.useOSProber = true;
-
-  # Use the NixOs boot loader.
-  boot.loader.systemd-boot.enable = true;
+  # Use the systemd-boot EFI boot loader.
+  # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "fixe-bureau"; # Define your hostname.
   # Pick only one of the below networking options.
--- nixos/fixe-salon/configuration.nix
+++ nixos/fixe-salon/configuration.nix
@@ -11,18 +11,13 @@
       ../software-configuration.nix
     ];
 
-  # # Use the grub EFI boot loader.
-  # boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.version = 2;
-  # boot.loader.grub.device = "nodev";
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.useOSProber = true;
-
-  # Use the NixOs boot loader.
-  boot.loader.systemd-boot.enable = true;
+  # Use the grub EFI boot loader.
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "fixe-salon"; # Define your hostname.
   # Pick only one of the below networking options.
