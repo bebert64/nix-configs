5a2ffde7ce2dfba30d317d830ce042052a982e9c
RomainC <bebert64@gmail.com>
2023-09-12 08:42:22 +0200

    use nixos bootloader (faster builds)

--- nixos/fixe-salon/configuration.nix
+++ nixos/fixe-salon/configuration.nix
@@ -11,13 +11,18 @@
       ../software-configuration.nix
     ];
 
-  # Use the grub EFI boot loader.
+  # # Use the grub EFI boot loader.
+  # boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.version = 2;
+  # boot.loader.grub.device = "nodev";
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.useOSProber = true;
+
+  # Use the NixOs boot loader.
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   networking.hostName = "fixe-salon"; # Define your hostname.
   # Pick only one of the below networking options.
