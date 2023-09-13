65ef50f43b3501c9381767face24d15e4d94458c
RomainC <bebert64@gmail.com>
2023-09-13 22:05:01 +0200

    update install_arch script

--- install_arch.sh
+++ install_arch.sh
@@ -5,23 +5,28 @@
 # mount /def/sda3 /mnt/boot --mkdir
 # swapon /dev/sda2
 # reflector
-# pacstrap -K /mnt base linux linux-firmware vim git sudo
+# pacstrap -K /mnt base linux linux-firmware vim git sudo grub efibootmgr
 # genfstab -U /mnt >> /mnt/etc/fstab
 # arch-chroot /mnt
 # passwd
 # useradd -m -G wheel romain
 # passwd romain
+# # uncomment wheel line
+# visudo
 # su romain
 # cd
-
-
-sudo ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-sudo hwclock --systohc
-sudo sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
-sudo sed -i 's/#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/g' /etc/locale.gen
-locale-gen
-sudo echo "LANG=en_US.UTF-8" > /etc/locale.conf
-sudo echo "KEYMAP=fr" > /etc/vconsole.conf
-sudo echo "fixe-bureau" > /etc/hostname
 # git clone https://github.com/bebert64/nix-configs
+# cd nix-configs
+# sudo ./install_user_config.sh
+
+
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+sed -i 's/#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "KEYMAP=fr" > /etc/vconsole.conf
+echo "fixe-bureau" > /etc/hostname
+
 echo "you can now reboot"
