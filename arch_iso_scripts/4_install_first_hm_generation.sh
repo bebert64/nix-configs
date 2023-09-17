06a0a49ddb2be957ccaac2dbdc9df15d279075ef
RomainC <bebert64@gmail.com>
2023-09-17 09:23:52 +0200

    don't need to modify /etc/profile

--- arch_iso_scripts/4_install_first_hm_generation.sh
+++ arch_iso_scripts/4_install_first_hm_generation.sh
@@ -12,16 +12,6 @@ cd ~
 git clone https://github.com/bebert64/nix-configs
 cd nix-configs/home-manager && home-manager switch --flake .#$FLAKE_CONFIG_NAME --extra-experimental-features nix-command --extra-experimental-features flakes
 
-# Needed for rofi to find apps and for all apps to find icons / cursors / etc...
-sudo bash -c 'cat >> /etc/profile << EOL
-if [ -f \$HOME/.nix-profile/etc/profile.d/nix.sh ];
-then
-     source \$HOME/.nix-profile/etc/profile.d/nix.sh
-fi
-
-export XDG_DATA_DIRS=\$HOME/.nix-profile/share:/usr/local/share:/usr/share:\$HOME/.local/share:$XDG_DATA_DIRS
-EOL'
-
 # Make zsh default shell
 sudo bash -c 'echo $(which zsh) >> /etc/shells'
 chsh -s $(which zsh)
