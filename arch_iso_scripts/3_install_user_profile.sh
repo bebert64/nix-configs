3c3e1b7608cf31042dad9e2e417e75c4e95f6eb2
RomainC <bebert64@gmail.com>
2023-09-22 11:40:37 +0200

    install video downloader companion app

--- arch_iso_scripts/3_install_user_profile.sh
+++ arch_iso_scripts/3_install_user_profile.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-cd
+cd ~
 rm -rf yay
 
 # Install user packages
@@ -14,7 +14,18 @@ yay -Syu xorg-server xorg-xinit xdg-utils fontconfig nfs-utils polkit alock udis
 # Install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+# Install video downloader companion app
+cd ~
+curl -L https://github.com/mi-g/vdhcoapp/releases/download/v1.6.3/net.downloadhelper.coapp-1.6.3-1_amd64.tar.gz -o ~/net.downloadhelper.coapp-1.6.3-1_amd64.tar.gz
+mkdir ~/.dwhelper
+tar xf net.downloadhelper.coapp-1.6.3-1_amd64.tar.gz -C ~/.dwhelper
+mv -v ~/.dwhelper/net.downloadhelper.coapp-1.6.3/* ~/.dwhelper
+rm -r ~/.dwhelper/net.downloadhelper.coapp-1.6.3
+~/.dwhelper/bin/net.downloadhelper.coapp-linux-64 install --user
+rm ~/net.downloadhelper.coapp-1.6.3-1_amd64.tar.gz
+
 # install nix
+cd ~
 curl --proto '=https' --tlsv1.2 -sSfL https://nixos.org/nix/install -o nix_install.sh
 chmod +x ./nix_install.sh
 ./nix_install.sh --daemon
