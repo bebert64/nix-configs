5312835659a4bd74787b0a1e0a357be9940cdb83
RomainC <bebert64@gmail.com>
2023-09-22 17:07:40 +0200

    move_to_ipad as nix script

--- dotfiles/move_to_ipad.sh
+++
@@ -1,8 +0,0 @@
-for n in $(seq 1 $#); do
-  echo $1
-  mkdir zip
-  zip "zip/$1.zip" "$1"/*
-  rsync --progress "zip/$1.zip" /home/romain/Mnt/Ipad/Chunky
-  rsync --progress "$1" /home/romain/Mnt/Ipad/Chunky
-  shift
-done
