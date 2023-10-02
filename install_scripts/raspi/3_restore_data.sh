3195cc803d13b66ea43226804fef4f74da6063e2
RomainC <bebert64@gmail.com>
2023-10-02 10:50:11 +0200

    fix restore_data script for raspberry

--- install_scripts/raspi/3_restore_data.sh
+++ install_scripts/raspi/3_restore_data.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 # Restore stash from nas backup
-sudo pkill stash
-sudo rm /usr/local/etc/stash/stash-go.sqlite
-sudo cp -r  rsync -avh /mnt/NAS/Comics/Fini/Planet\ of\ the\ Apes/14\ Planet\ of\ the\ Apes\ issues/Elseworlds/stash_bkp/ /usr/local/etc/stash
+sudo pkill stash || true
+sudo rm -f /usr/local/etc/stash/stash-go.sqlite
+sudo rsync -avh /mnt/NAS/Comics/Fini/Planet\ of\ the\ Apes/14\ Planet\ of\ the\ Apes\ issues/Elseworlds/stash_bkp/ /usr/local/etc/stash
 sudo stash-linux-arm64v8 --nobrowser --config /usr/local/etc/stash/config.yml &
 
 # Restore postgres data
