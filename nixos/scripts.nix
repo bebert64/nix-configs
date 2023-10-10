8d43f7d79dc4f00043a308b450fc7a0498b3c046
RomainC <bebert64@gmail.com>
2023-10-10 16:50:46 +0200

    simplify lock-conky for nixos config

--- nixos/scripts.nix
+++ nixos/scripts.nix
@@ -120,49 +120,38 @@ host-specific: { pkgs, ...}:
 
     (pkgs.writeScriptBin "lock-conky" ''
     #!/usr/bin/env bash
+    set -euxo pipefail
 
-      while getopts "s" opt; do
-            case $opt in
-                  s) sleep=true;;
-                  \?) echo "Invalid option. To sleep, use -s";;
-            esac
-      done
+    SLEEP=false
+    while getopts "s" opt; do
+      case $opt in
+        s) SLEEP=true;;
+        \?) echo "Invalid option. To sleep, use -s";;
+      esac
+    done
 
-      THEME=$(ls $HOME/.conky/ | sort -R | tail -1)
-      SCREEN_OFF=$(xrandr --query | grep connected | grep -v primary | cut -d ' ' -f 1  )
+    # Prepare screen
+    pkill polybar || echo "polybar already killed"
+    wk1=$(i3-msg -t get_workspaces | jq '.[] | select(.visible==true).name' | head -1)
+    wk2=$(i3-msg -t get_workspaces | jq '.[] | select(.visible==true).name' | tail -1)
+    i3-msg "workspace \" \"; workspace \"  \""
 
-      # Prepare screen
-      pkill xidlehook
-      pkill polybar
-      wk1=$(i3-msg -t get_workspaces | jq '.[] | select(.visible==true).name' | head -1)
-      wk2=$(i3-msg -t get_workspaces | jq '.[] | select(.visible==true).name' | tail -1)
-      i3-msg "workspace \" \"; workspace \"  \""
-      $HOME/.conky/$THEME/launch.sh
-      xrandr --output $SCREEN_OFF --brightness 0
+    # Sleep or prepare to sleep
+    if [[ $SLEEP == true ]]; then
+      systemctl suspend
+    else
+      pkill xidlehook || echo "xidlehook already killed"
+      xidlehook --timer ${toString (host-specific.minutes-from-lock-to-sleep * 60)} 'systemctl suspend' ' ' &
+    fi
+    
+    # Lock
+    alock -bg none -cursor blank
 
-
-      # Sleep or prepare to sleep
-      if [ $sleep ]; then
-            systemctl suspend
-      else
-            xidlehook --timer ${toString (host-specific.minutes-from-lock-to-sleep * 60)} 'systemctl suspend' ' ' &
-      fi
-      
-      # Lock
-      alock -bg none -cursor blank
-
-      # Wake up
-      if [ ! $sleep ]; then
-            pkill xidlehook
-      fi
-
-      # Revert to original config
-      i3-msg workspace "$wk1"
-      i3-msg workspace "$wk2"
-      $HOME/.fehbg
-      pkill conky
-      $HOME/.config/polybar/launch.sh
-      xidlehook --timer ${toString (host-specific.minutes-before-lock * 60)} 'lock-conky' ' ' &
-      xrandr --output $SCREEN_OFF --brightness 1
+    # Revert to original config
+    i3-msg workspace "$wk1"
+    i3-msg workspace "$wk2"
+    $HOME/.config/polybar/launch.sh
+    pkill xidlehook || echo "xidlehook already killed"
+    xidlehook --timer ${toString (host-specific.minutes-before-lock * 60)} 'lock-conky' ' ' &
     '')
 ]
