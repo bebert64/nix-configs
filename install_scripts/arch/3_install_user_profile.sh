08fdf0a70665c98e7b9f2ad8dfb9c2e6f86dd900
RomainC <bebert64@gmail.com>
2024-03-06 13:16:02 +0100

    update dwhleper config

--- install_scripts/arch/3_install_user_profile.sh
+++ install_scripts/arch/3_install_user_profile.sh
@@ -1,35 +1,36 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 set -euo pipefail
 
-# install yay
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ~
-rm -rf yay
+# # install yay
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si
+# cd ~
+# rm -rf yay
 
-# Install user packages
-yay -Syu xorg-server xorg-xinit xdg-utils fontconfig nfs-utils polkit alock udisks2 picom postgresql-libs
+# # Install user packages
+# yay -Syu xorg-server xorg-xinit xdg-utils fontconfig nfs-utils polkit alock udisks2 picom postgresql-libs
 
-# Install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# # Install rust
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install video downloader companion app
 cd ~
-curl -L https://github.com/mi-g/vdhcoapp/releases/download/v1.6.3/net.downloadhelper.coapp-1.6.3-1_amd64.tar.gz -o ~/net.downloadhelper.coapp-1.6.3-1_amd64.tar.gz
+curl -L https://github.com/aclap-dev/vdhcoapp/releases/download/v2.0.10/vdhcoapp-2.0.10-linux-x86_64.tar.bz2 -o ~/net.downloadhelper.coapp-2.0.10_amd64.tar.gz
+rm -rf ~/.dwhelper
 mkdir ~/.dwhelper
-tar xf net.downloadhelper.coapp-1.6.3-1_amd64.tar.gz -C ~/.dwhelper
-mv -v ~/.dwhelper/net.downloadhelper.coapp-1.6.3/* ~/.dwhelper
-rm -r ~/.dwhelper/net.downloadhelper.coapp-1.6.3
-~/.dwhelper/bin/net.downloadhelper.coapp-linux-64 install --user
-rm ~/net.downloadhelper.coapp-1.6.3-1_amd64.tar.gz
+tar xf net.downloadhelper.coapp-2.0.10_amd64.tar.gz -C ~/.dwhelper
+mv -v ~/.dwhelper/vdhcoapp-2.0.10/* ~/.dwhelper
+rm -r ~/.dwhelper/vdhcoapp-2.0.10
+~/.dwhelper/vdhcoapp install --user
+rm ~/net.downloadhelper.coapp-2.0.10_amd64.tar.gz
 
-# install nix
-cd ~
-curl --proto '=https' --tlsv1.2 -sSfL https://nixos.org/nix/install -o nix_install.sh
-chmod +x ./nix_install.sh
-./nix_install.sh --daemon
-rm nix_install.sh
+# # install nix
+# cd ~
+# curl --proto '=https' --tlsv1.2 -sSfL https://nixos.org/nix/install -o nix_install.sh
+# chmod +x ./nix_install.sh
+# ./nix_install.sh --daemon
+# rm nix_install.sh
 
-# Need to reboot for nix to load correctly
-echo "you should now reboot"
+# # Need to reboot for nix to load correctly
+# echo "you should now reboot"
