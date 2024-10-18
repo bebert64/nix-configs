2c168a1123afda9b5dfb17b1bb0d365310f57ddc
Your Name <you@example.com>
2024-10-18 14:40:55 +0200

    one flake

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "NixOS and HomeManager configurations";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: 
+    let 
+      raspi = "raspi";
+      fixe-bureau = "fixe-bureau";
+      config = {
+        allowUnfree = true;
+      };
+    in {
+      nixosConfigurations = {
+        stockly-romainc = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+            home-manager.nixosModules.home-manager
+            ./nixos/stockly-romainc/configuration.nix
+            ];
+            specialArgs = {
+            flake-inputs = inputs;
+            host-specific = import ./nixos/stockly-romainc/host-specific.nix;
+            };
+        };
+      };
+        homeConfigurations = {
+            ${raspi} = home-manager.lib.homeManagerConfiguration rec {
+                pkgs = import nixpkgs {
+                    system = "aarch64-linux";  # x86_64-linux, aarch64-multiplatform, etc.
+                    inherit config;
+                };
+
+                modules = [
+                    (import ./home-manager/home_raspi.nix ( { config-name = raspi; inherit pkgs; }))
+                ];
+            };
+
+            ${fixe-bureau} = home-manager.lib.homeManagerConfiguration rec {
+                pkgs = import nixpkgs {
+                    system = "x86_64-linux";  # x86_64-linux, aarch64-multiplatform, etc.
+                    inherit config;
+                };
+
+                modules = 
+                    let
+                        host-specifics = import ./home-manager/fixe-bureau/host_specifics.nix;
+                    in [
+                        (import ./home-manager/home.nix ( { 
+                            config-name = fixe-bureau; 
+                            inherit pkgs config host-specifics; 
+                            lib=nixpkgs.lib; 
+                            hm-lib=home-manager.lib;
+                        }))
+                    ];
+            };
+        };
+  };
+}
