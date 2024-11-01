c9141a01b8d53c0e3fcf416cdc46b8991f8dd3d1
RomainC <bebert64@gmail.com>
2024-11-01 18:18:24 +0100

    merge configuration.nix files

--- nix/nixos/stockly-romainc/configuration.nix
+++ nix/nixos/stockly-romainc/configuration.nix
@@ -1,6 +1,16 @@
 { pkgs, specialArgs, ... }:
 {
-  imports = [ ../common.nix ];
+  imports = [ ../common.nix 
+    ./hardware-configuration.nix];
+
+  # Configuration options that are not standard NixOS, but were defined by Stockly
+  stockly = {
+    acer-quanta-webcam-fix = true;
+    ui = null;
+    nvidia = false;
+    auto-update.enable = true; # See auto-update.nix for doc
+    keyboard-layout = "fr";
+  };
 
   home-manager = {
     users.user.imports = [ ../home.nix ];
@@ -26,4 +36,11 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
