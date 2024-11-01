045f4ca0a450ab609e2298983367ceb9d628fdba
RomainC <bebert64@gmail.com>
2024-11-01 18:13:23 +0100

    simplify stockly's configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,24 +1,4 @@
 {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-    ../nix/nixos/stockly-romainc/configuration.nix
-  ];
-
-  # Configuration options that are not standard NixOS, but were defined by Stockly
-  stockly = {
-    acer-quanta-webcam-fix = true;
-    ui = null;
-    nvidia = false;
-    auto-update.enable = true; # See auto-update.nix for doc
-    keyboard-layout = "fr";
-  };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  # This file is needed because an activation script from stockly tries to read it...
+  acer-quanta-webcam-fix = true;
 }
