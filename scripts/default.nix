fa0f07b7180cb7d4dc7495df63f05aee93cf0542
Your Name <you@example.com>
2024-11-04 09:47:17 +0100

    wip

--- scripts/default.nix
+++ scripts/default.nix
@@ -105,59 +105,53 @@
   echo "start"
 echo $(psg strawberry)
 echo "Moving on"
-      IS_STRAWBERRY_LAUNCHED="not launched"
+      IS_STRAWBERRY_LAUNCHED=$(echo "test " $(psg strawberry))
   echo "launched :"
   echo $IS_STRAWBERRY_LAUNCHED
-      IS_STRAWBERRY_LAUNCHED=$(echo "bonjour")
-  echo "launched :"
-  echo $IS_STRAWBERRY_LAUNCHED
-      IS_STRAWBERRY_LAUNCHED=$(psg strawberry)
-  echo "launched :"
-  echo $IS_STRAWBERRY_LAUNCHED
-    strawberry &
-    sleep 2
-      IS_STRAWBERRY_LAUNCHED=$(psg strawberry)
-  echo "launched again:"
-  echo $IS_STRAWBERRY_LAUNCHED
+  #   strawberry &
+  #   sleep 2
+  #     IS_STRAWBERRY_LAUNCHED=$(psg strawberry)
+  # echo "launched again:"
+  # echo $IS_STRAWBERRY_LAUNCHED
 
 
-    play_radio() {
-    echo "starting"
-      IS_STRAWBERRY_LAUNCHED=$(psg strawberry)
-  echo "launched :"
-  echo $IS_STRAWBERRY_LAUNCHED
+  #   play_radio() {
+  #   echo "starting"
+  #     IS_STRAWBERRY_LAUNCHED=$(psg strawberry)
+  # echo "launched :"
+  # echo $IS_STRAWBERRY_LAUNCHED
 
-      if [[ ! $IS_STRAWBERRY_LAUNCHED ]]; then
-      echo "launching"
-          strawberry &
-      fi
+  #     if [[ ! $IS_STRAWBERRY_LAUNCHED ]]; then
+  #     echo "launching"
+  #         strawberry &
+  #     fi
 
-      while [[ ! $IS_STRAWBERRY_LAUNCHED ]]; do
-          IS_STRAWBERRY_LAUNCHED=$(psg strawberry)
-          sleep 1
-      done
+  #     while [[ ! $IS_STRAWBERRY_LAUNCHED ]]; do
+  #         IS_STRAWBERRY_LAUNCHED=$(psg strawberry)
+  #         sleep 1
+  #     done
 
-      strawberry --play-playlist Radios &
-      strawberry --play-track $1 &
-    }
+  #     strawberry --play-playlist Radios &
+  #     strawberry --play-track $1 &
+  #   }
 
-    MENU="$(echo -en \
-    'FIP\0icon\x1f${../assets/icons/fip.png}
-    Jazz Radio\0icon\x1f${../assets/icons/jazz-radio.jpg}
-    Radio Nova\0icon\x1f${../assets/icons/nova.jpg}
-    Oui FM\0icon\x1f${../assets/icons/Oui-FM.png}
-    Classic FM\0icon\x1f${../assets/icons/classic-FM.png}
-    Chillhop Radio\0icon\x1f${../assets/icons/chillhop.jpg}' \
-    | rofi -dmenu -show-icons -i -p 'Radio')"
+  #   MENU="$(echo -en \
+  #   'FIP\0icon\x1f${../assets/icons/fip.png}
+  #   Jazz Radio\0icon\x1f${../assets/icons/jazz-radio.jpg}
+  #   Radio Nova\0icon\x1f${../assets/icons/nova.jpg}
+  #   Oui FM\0icon\x1f${../assets/icons/Oui-FM.png}
+  #   Classic FM\0icon\x1f${../assets/icons/classic-FM.png}
+  #   Chillhop Radio\0icon\x1f${../assets/icons/chillhop.jpg}' \
+  #   | rofi -dmenu -show-icons -i -p 'Radio')"
 
-    case "$MENU" in
-      FIP) play_radio 0 ;;
-      "Jazz Radio") play_radio 1 ;;
-      "Radio Nova") play_radio 2 ;;
-      "Oui FM") play_radio 3 ;;
-      "Classic FM") play_radio 4 ;;
-      "Chillhop Radio") i3-msg "workspace 10:; exec firefox -new-window https://www.youtube.com/watch\?v\=5yx6BWlEVcY" ;;
-    esac
+  #   case "$MENU" in
+  #     FIP) play_radio 0 ;;
+  #     "Jazz Radio") play_radio 1 ;;
+  #     "Radio Nova") play_radio 2 ;;
+  #     "Oui FM") play_radio 3 ;;
+  #     "Classic FM") play_radio 4 ;;
+  #     "Chillhop Radio") i3-msg "workspace 10:; exec firefox -new-window https://www.youtube.com/watch\?v\=5yx6BWlEVcY" ;;
+  #   esac
   '')
 
   (pkgs.writeScriptBin "lock-conky" ''
