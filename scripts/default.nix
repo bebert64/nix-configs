429df00fb4f3e2104e2e349b2332938fdfc3aead
Your Name <you@example.com>
2024-11-04 09:56:06 +0100

    wip

--- scripts/default.nix
+++ scripts/default.nix
@@ -135,25 +135,25 @@ echo "Moving on"
       strawberry --play-track $1 &
     }
 
-    play_radio 1
+    # play_radio 1
 
-  #   MENU="$(echo -en \
-  #   'FIP\0icon\x1f${../assets/icons/fip.png}
-  #   Jazz Radio\0icon\x1f${../assets/icons/jazz-radio.jpg}
-  #   Radio Nova\0icon\x1f${../assets/icons/nova.jpg}
-  #   Oui FM\0icon\x1f${../assets/icons/Oui-FM.png}
-  #   Classic FM\0icon\x1f${../assets/icons/classic-FM.png}
-  #   Chillhop Radio\0icon\x1f${../assets/icons/chillhop.jpg}' \
-  #   | rofi -dmenu -show-icons -i -p 'Radio')"
+    MENU="$(echo -en \
+    'FIP\0icon\x1f${../assets/icons/fip.png}
+    Jazz Radio\0icon\x1f${../assets/icons/jazz-radio.jpg}
+    Radio Nova\0icon\x1f${../assets/icons/nova.jpg}
+    Oui FM\0icon\x1f${../assets/icons/Oui-FM.png}
+    Classic FM\0icon\x1f${../assets/icons/classic-FM.png}
+    Chillhop Radio\0icon\x1f${../assets/icons/chillhop.jpg}' \
+    | rofi -dmenu -show-icons -i -p 'Radio')"
 
-  #   case "$MENU" in
-  #     FIP) play_radio 0 ;;
-  #     "Jazz Radio") play_radio 1 ;;
-  #     "Radio Nova") play_radio 2 ;;
-  #     "Oui FM") play_radio 3 ;;
-  #     "Classic FM") play_radio 4 ;;
-  #     "Chillhop Radio") i3-msg "workspace 10:; exec firefox -new-window https://www.youtube.com/watch\?v\=5yx6BWlEVcY" ;;
-  #   esac
+    case "$MENU" in
+      FIP) play_radio 0 ;;
+      "Jazz Radio") play_radio 1 ;;
+      "Radio Nova") play_radio 2 ;;
+      "Oui FM") play_radio 3 ;;
+      "Classic FM") play_radio 4 ;;
+      "Chillhop Radio") i3-msg "workspace 10:; exec firefox -new-window https://www.youtube.com/watch\?v\=5yx6BWlEVcY" ;;
+    esac
   '')
 
   (pkgs.writeScriptBin "lock-conky" ''
