f9917a9122b1e4dc51ab26eef4294b1831469a92
RomainC <bebert64@gmail.com>
2024-11-16 14:14:24 +0100

    wip

--- programs/email.nix
+++ programs/email.nix
@@ -1,39 +1,53 @@
-{ config
-, lib
-, ...
-}:
+{ config, lib, ... }:
 let
   modifier = config.xsession.windowManager.i3.config.modifier;
 in
 {
-  accounts.email.accounts.bebert64 = {
-    address = "bebert64@gmail.com";
-    aliases = [ "romain.cassan.64@gmail.com" ];
-    flavor = "gmail.com";
-    primary = true;
-    realName = "Romain Cassan";
-    thunderbird = {
-      enable = true;
-
+  accounts.email.accounts = {
+    bebert64 = {
+      primary = true;
+      address = "bebert64@gmail.com";
+      aliases = [ "romain.cassan.64@gmail.com" ];
+      flavor = "gmail.com";
+      realName = "Romain Cassan";
+      thunderbird = {
+        enable = true;
+      };
     };
-    # getmail = {
-    #   enable = true;
-    #   delete = true;
-    # };
+    stockly = {
+      address = "romain@stockly.ai";
+      flavor = "gmail.com";
+      realName = "Romain Cassan";
+      thunderbird = {
+        enable = true;
+      };
+    };
   };
 
   programs.thunderbird = {
     enable = true;
-    profiles.default.isDefault = true;
-
+    profiles.default = {
+      isDefault = true;
+      # Necessary for gmail oauth2 to work
+      settings = {
+        # For bebert64
+        "mail.server.server_e834497f008a126ed365f02d8412dac1c4c265c127b9bccda5022be9506c5dba.authMethod" =
+          10;
+        "mail.smtpserver.smtp_e834497f008a126ed365f02d8412dac1c4c265c127b9bccda5022be9506c5dba.authMethod" =
+          10;
+        # For stockly
+        "mail.server.server_cbaa1f5c537cd000837507ad54378a8babf63212057c78457d0a07bd8dfe4e17.authMethod" =
+          10;
+        "mail.smtpserver.smtp_cbaa1f5c537cd000837507ad54378a8babf63212057c78457d0a07bd8dfe4e17.authMethod" =
+          10;
+      };
+    };
   };
 
   xsession.windowManager.i3.config = {
     assigns = {
-      "$ws5" = [{ class = "thunderbird"; }];
+      "$ws5" = [ { class = "thunderbird"; } ];
     };
-    keybindings = lib.mkOptionDefault {
-      "${modifier}+Control+t" = "workspace $ws5; exec thunderbird";
-    };
+    keybindings = lib.mkOptionDefault { "${modifier}+Control+t" = "workspace $ws5; exec thunderbird"; };
   };
 }
