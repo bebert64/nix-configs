58bcc1a75653f6c1b85bd6056fdcdf5416f44e8e
RomainC <bebert64@gmail.com>
2024-11-16 13:55:31 +0100

    wip

--- programs/email.nix
+++ programs/email.nix
@@ -1,18 +1,15 @@
-{
-  config,
-  lib,
-  ...
+{ config
+, lib
+, ...
 }:
 let
   modifier = config.xsession.windowManager.i3.config.modifier;
 in
 {
-  accounts.email.accounts = {
-    name = "bebert64";
+  accounts.email.accounts.bebert64 = {
     address = "bebert64@gmail.com";
     aliases = [ "romain.cassan.64@gmail.com" ];
     flavor = "gmail.com";
-    passwordCommand = "sops-read gmail/bebert64";
     primary = true;
     realName = "Romain Cassan";
     thunderbird = {
@@ -27,18 +24,16 @@ in
 
   programs.thunderbird = {
     enable = true;
-    profiles.default = {
-      name = "default";
-      isDefault = true;
-    };
+    profiles.default.isDefault = true;
+
   };
 
   xsession.windowManager.i3.config = {
     assigns = {
-      "$ws5" = [ { class = "thunderbird"; } ];
+      "$ws5" = [{ class = "thunderbird"; }];
     };
     keybindings = lib.mkOptionDefault {
-      "${modifier}+Control+t" = "workspace $ws5; exec thunderbird -P Regular";
+      "${modifier}+Control+t" = "workspace $ws5; exec thunderbird";
     };
   };
 }
