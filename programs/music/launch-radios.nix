179e387d34c665189cfa31c1851912574578b7ae
RomainC <bebert64@gmail.com>
2024-11-18 11:15:42 +0100

    wip

--- programs/music/launch-radios.nix
+++ programs/music/launch-radios.nix
@@ -9,7 +9,7 @@ in
         ps aux | grep $1 | grep -v grep
       }
 
-      play-radio() {
+      play_radio() {
         IS_STRAWBERRY_LAUNCHED=$(psg strawberry)
 
         if [[ ! $IS_STRAWBERRY_LAUNCHED ]]; then
@@ -28,29 +28,29 @@ in
       MENU="$(echo -en \
       'FIP\0icon\x1f${./icons/fip.png}
       FIP Jazz\0icon\x1f${./icons/fip-jazz.png}
-      FIP Rock\0icon\x1f${./icons/fip-rock.png}
-      FIP Groove\0icon\x1f${./icons/fip.png}
-      FIP Reggae\0icon\x1f${./icons/fip.png}
-      FIP Pop\0icon\x1f${./icons/fip.png}
-      FIP Monde\0icon\x1f${./icons/fip.png}
-      FIP Nouveautés\0icon\x1f${./icons/fip.png}
+      FIP Rock\0icon\x1f${./icons/fip-rock.jpg}
+      FIP Groove\0icon\x1f${./icons/fip-groove.jpg}
+      FIP Reggae\0icon\x1f${./icons/fip-reggae.jpg}
+      FIP Pop\0icon\x1f${./icons/fip-pop.jpg}
+      FIP Monde\0icon\x1f${./icons/fip-monde.jpg}
+      FIP Nouveautés\0icon\x1f${./icons/fip-nouveautes.jpg}
       Radio Nova\0icon\x1f${./icons/nova.jpg}
-      Radio Swiss Classic\0icon\x1f${./icons/fip.png}
+      Radio Swiss Classic\0icon\x1f${./icons/radio-swiss-classic.png}
       Chillhop Music\0icon\x1f${./icons/chillhop.jpg}' \
       | rofi -dmenu -show-icons -i -p 'Radio')"
 
       case "$MENU" in
-        FIP) play-radio 0 ;;
-        "FIP Jazz") play-radio 1 ;;
-        "FIP Rock") play-radio 2 ;;
-        "FIP Groove") play-radio 3 ;;
-        "FIP Reggae") play-radio 4 ;;
-        "FIP Pop") play-radio 5 ;;
-        "FIP Monde") play-radio 6 ;;
-        "FIP Nouveautés") play-radio 7 ;;
-        "Radio Nova") play-radio 8 ;;
-        "Radio Swiss Classic") play-radio 9 ;;
-        "Chillhop Music") play-radio 10 ;;
+        FIP) play_radio 0 ;;
+        "FIP Jazz") play_radio 1 ;;
+        "FIP Rock") play_radio 2 ;;
+        "FIP Groove") play_radio 3 ;;
+        "FIP Reggae") play_radio 4 ;;
+        "FIP Pop") play_radio 5 ;;
+        "FIP Monde") play_radio 6 ;;
+        "FIP Nouveautés") play_radio 7 ;;
+        "Radio Nova") play_radio 8 ;;
+        "Radio Swiss Classic") play_radio 9 ;;
+        "Chillhop Music") play_radio 10 ;;
       esac
     '')
   ];
