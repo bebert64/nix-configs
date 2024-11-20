bc049cbd9c57d205a06c59592b296ffd20c3d571
RomainC <bebert64@gmail.com>
2024-11-20 17:50:04 +0100

    wip

--- programs/stash/default.nix
+++ programs/stash/default.nix
@@ -1,11 +1,16 @@
-{ nixpkgs-unstable, pkgs, config, ... }:
+# Waiting for stash version from nixpkgs to catch up with the installed version
+# so that the schema is compatible with my current db.
+# { nixpkgs-unstable, pkgs, config, ... }:
+{ pkgs, config, ... }:
 let
-  overlay-unstable = final: prev: {
-    unstable = nixpkgs-unstable.legacyPackages.aarch64-linux;
-  };
+  hmCfg = config.home-manager.users.${config.by-db.username};
+  nixConfigsRepo = hmCfg.by-db.nixConfigsRepo;
+  #   overlay-unstable = final: prev: {
+  #     unstable = nixpkgs-unstable.legacyPackages.aarch64-linux;
+  #   };
 in
 {
-  nixpkgs.overlays = [ overlay-unstable ];
+  # nixpkgs.overlays = [ overlay-unstable ];
 
   systemd = {
     services = {
@@ -23,8 +28,8 @@ in
 
   system.activationScripts = {
     symlingStashConfig = ''
-      # ${pkgs.coreutils}/bin/mkdir -p /root/.stash
-      ${pkgs.coreutils}/bin/cp -s /home/romain/nix-configs/programs/stash/config.yml /stash/config.yml
+      ${pkgs.coreutils}/bin/mkdir -p /stash
+      ${pkgs.coreutils}/bin/ln -s ${nixConfigsRepo}/programs/stash/config.yml /stash/config.yml
     '';
   };
 
