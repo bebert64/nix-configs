2851ecbac79b14aaa764f2a748667f05f886c268
RomainC <bebert64@gmail.com>
2024-11-20 10:49:14 +0100

    wip

--- programs/stash/default.nix
+++ programs/stash/default.nix
@@ -1,11 +1,41 @@
 { nixpkgs-unstable, pkgs, ... }:
 let
   overlay-unstable = final: prev: {
-    unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
+    unstable = nixpkgs-unstable.legacyPackages.aarch64-linux;
   };
 in
 {
   home.packages = [ pkgs.unstable.stash ];
 
   nixpkgs.overlays = [ overlay-unstable ];
+
+  # systemd.user =  {
+  #           enable = true;
+  #           services = {
+  #             test = {
+  #               Unit = {
+  #                 Description = "Chooses walpaper(s) based on the number of monitors connected";
+  #               };
+  #               Service = {
+  #                 Type = "exec";
+  #                 ExecStart = "${package}/bin/wallpapers-manager change ${cfg.service.commandArgs}";
+  #               };
+
+  #             };
+  #           };
+  #           timers = {
+  #             wallpapers-manager = {
+  #               Unit = {
+  #                 Description = "Timer for wallpapers-manager";
+  #               };
+  #               Timer = {
+  #                 Unit = "wallpapers-manager.service";
+  #                 OnUnitInactiveSec = "1h";
+  #               };
+  #               Install = {
+  #                 WantedBy = [ "timers.target" ];
+  #               };
+  #             };
+  #           };
+  #         };
 }
