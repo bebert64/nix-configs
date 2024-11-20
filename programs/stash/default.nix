d081751be03765f5b93adb9a492b3ba23b93c514
RomainC <bebert64@gmail.com>
2024-11-20 11:57:16 +0100

    wip

--- programs/stash/default.nix
+++ programs/stash/default.nix
@@ -5,8 +5,6 @@ let
   };
 in
 {
-  # environment.systemPackages = [ pkgs.unstable.stash ];
-
   nixpkgs.overlays = [ overlay-unstable ];
 
   systemd = {
@@ -17,25 +15,15 @@ in
         wantedBy = [ "multi-user.target" ];
         serviceConfig = {
           Type = "exec";
-          ExecStart = "${pkgs.unstable.stash}/bin/stash";
+          ExecStart = "${pkgs.unstable.stash}/bin/stash --config /usr/local/etc/stash/config.yml";
         };
-
       };
     };
-    # timers = {
-    #   stash = {
-    #     Unit = {
-    #       Description = "Timer for test";
-    #     };
-    #     Timer = {
-    #       Unit = "test.service";
-    #       OnBootSec = "1s";
-    #       OnUnitInactiveSec = "5";
-    #     };
-    #     Install = {
-    #       WantedBy = [ "timers.target" ];
-    #     };
-    #   };
-    # };
+  };
+
+  system.activationScripts = {
+    symlingStashConfig = ''
+      ln -s ${./config.yml} /usr/local/etc/stash/
+    '';
   };
 }
