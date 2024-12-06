410dd4cb37b6e0768cfd5b9619080eea43ed9f4a
RomainC <bebert64@gmail.com>
2024-12-06 09:05:21 +0100

    wip

--- nixos/server.nix
+++ nixos/server.nix
@@ -84,7 +84,23 @@
             proxy_read_timeout 60000s;
           '';
         };
-
+      };
+      services.nginx.virtualHosts."freebox.capucina.house" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://192.168.1.254";
+          # extraConfig = ''
+          #   proxy_http_version 1.1;
+          #   proxy_set_header Upgrade $http_upgrade;
+          #   proxy_set_header Connection "upgrade";
+          #   proxy_set_header Host $host;
+          #   proxy_set_header X-Real-IP $remote_addr;
+          #   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          #   proxy_set_header X-Forwarded-Proto $scheme;
+          #   proxy_read_timeout 60000s;
+          # '';
+        };
       };
       security.acme = {
         acceptTerms = true;
