263a7588d0f5ecce862cbd4a7ba6cbb8fc4148df
RomainC <bebert64@gmail.com>
2024-12-12 15:41:35 +0100

    wip

--- programs/configs/vscode/default.nix
+++ programs/configs/vscode/default.nix
@@ -6,6 +6,18 @@
 }:
 let
   modifier = config.xsession.windowManager.i3.config.modifier;
+  open-local = "${pkgs.writeScriptBin "open-local" ''
+    selection=$(${pkgs.fd}/bin/fd . --type dir --base-directory $HOME 2>/dev/null | \
+      rofi -sort -sorting-method fzf -disable-history -dmenu -show-icons -no-custom -p ""
+    )
+    code $HOME/$selection
+  ''}/bin/open-local";
+  open-remote = "${pkgs.writeScriptBin "open-remote" ''
+    selection=$(ssh cerberus "nix run \"nixpkgs#fd\" -- --base-directory ./Stockly/Main --type dir" 2>/dev/null | \
+        rofi -sort -sorting-method fzf -disable-history -dmenu -show-icons -no-custom -p ""
+      )
+      code --folder-uri=vscode-remote://ssh-remote+cerberus/home/romain/Stockly/Main/$selection
+  ''}/bin/open-remote";
 in
 {
   home = {
@@ -22,6 +34,9 @@ in
     assigns = {
       "$ws3" = [ { class = "Code"; } ];
     };
-    keybindings = lib.mkOptionDefault { "${modifier}+Control+v" = "workspace $ws3; exec code"; };
+    keybindings = lib.mkOptionDefault {
+      "${modifier}+Control+v" = "workspace $ws3; exec ${open-local}";
+      "${modifier}+Control+Shift+v" = "workspace $ws3; exec ${open-remote}";
+    };
   };
 }
