7d69c4ab5ff49788dc193429bffac38bbe43269c
RomainC <bebert64@gmail.com>
2025-07-28 17:30:34 +0200

    wip

--- programs/configs/jellyfin.nix
+++ programs/configs/jellyfin.nix
@@ -15,10 +15,9 @@
       enableACME = true;
       forceSSL = true;
       locations."/" = {
-        proxyPass = "http://127.0.0.1:8096";
+        proxyPass = "http://$jellyfin:8096";
         extraConfig = ''
           # Proxy main Jellyfin traffic
-          proxy_pass http://$jellyfin:8096;
           proxy_set_header Host $host;
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -28,6 +27,21 @@
 
           # Disable buffering when the nginx proxy gets very resource heavy upon streaming
           proxy_buffering off;
+        '';
+      };
+      location."/socket" = {
+        proxyPass = "http://$jellyfin:8096";
+        extraConfig = ''
+          # Proxy Jellyfin Websockets traffic
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "upgrade";
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_set_header X-Forwarded-Protocol $scheme;
+          proxy_set_header X-Forwarded-Host $http_host;
         '';
       };
     };
