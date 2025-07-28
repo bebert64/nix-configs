b1dfc68c5aaa983b3536ecd0c673daef81dfd839
RomainC <bebert64@gmail.com>
2025-07-28 17:34:47 +0200

    wip

--- programs/configs/jellyfin.nix
+++ programs/configs/jellyfin.nix
@@ -15,7 +15,7 @@
       enableACME = true;
       forceSSL = true;
       locations."/" = {
-        proxyPass = "http://$jellyfin:8096";
+        proxyPass = "http://127.0.0.1:8096";
         extraConfig = ''
           # Proxy main Jellyfin traffic
           proxy_set_header Host $host;
@@ -30,7 +30,7 @@
         '';
       };
       locations."/socket" = {
-        proxyPass = "http://$jellyfin:8096";
+        proxyPass = "http://127.0.0.1:8096";
         extraConfig = ''
           # Proxy Jellyfin Websockets traffic
           proxy_http_version 1.1;
