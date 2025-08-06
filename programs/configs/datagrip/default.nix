618e37b44829706d2a327de192d3b2e07940fd7e
RomainC <bebert64@gmail.com>
2025-08-06 19:42:32 +0200

    wip

--- programs/configs/datagrip/default.nix
+++ programs/configs/datagrip/default.nix
@@ -6,17 +6,18 @@
 }:
 let
   modifier = config.xsession.windowManager.i3.config.modifier;
-  nixConfigsRepo = "${config.home.homeDirectory}/${config.by-db.nixConfigsRepo}";
+  homeDir = config.home.homeDirectory;
+  nixConfigsRepo = "${homeDir}/${config.by-db.nixConfigsRepo}";
 in
 {
   home = {
     packages = [ (import ./jetbrains.nix { inherit lib pkgs; }).datagrip ];
     activation = {
       symlinkDatagripProfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        ln -sf ${nixConfigsRepo}/programs/configs/datagrip/datasources/perso/dataSources.xml $HOME/datagrip-projects/perso/.idea/dataSources.xml
-        ln -sf ${nixConfigsRepo}/programs/configs/datagrip/datasources/perso/dataSources.local.xml $HOME/datagrip-projects/perso/.idea/dataSources.local.xml
-        ln -sf ${nixConfigsRepo}/programs/configs/datagrip/datasources/Stockly/dataSources.xml $HOME/datagrip-projects/Stockly/.idea/dataSources.xml
-        ln -sf ${nixConfigsRepo}/programs/configs/datagrip/datasources/Stockly/dataSources.local.xml $HOME/datagrip-projects/Stockly/.idea/dataSources.local.xml
+        ln -sf ${nixConfigsRepo}/programs/configs/datagrip/datasources/perso/dataSources.xml ${homeDir}/datagrip-projects/perso/.idea/dataSources.xml
+        ln -sf ${nixConfigsRepo}/programs/configs/datagrip/datasources/perso/dataSources.local.xml ${homeDir}/datagrip-projects/perso/.idea/dataSources.local.xml
+        ln -sf ${nixConfigsRepo}/programs/configs/datagrip/datasources/Stockly/dataSources.xml ${homeDir}/datagrip-projects/Stockly/.idea/dataSources.xml
+        ln -sf ${nixConfigsRepo}/programs/configs/datagrip/datasources/Stockly/dataSources.local.xml ${homeDir}/datagrip-projects/Stockly/.idea/dataSources.local.xml
       '';
     };
   };
