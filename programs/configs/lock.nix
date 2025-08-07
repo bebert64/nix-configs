60d22b59d1f7db78f0dceea01793d43a54dda99e
RomainC <bebert64@gmail.com>
2025-08-07 21:07:13 +0200

    Add lock mode to i3, allowing to lock without sleeping or disable sleep entirely for 2 hours

--- programs/configs/lock.nix
+++ programs/configs/lock.nix
@@ -31,63 +31,77 @@ in
     };
   };
 
-  config = {
-    home.packages = [
-      alock
-      xidlehook
-      (writeScriptBin "lock-conky" ''
-        SLEEP=false
-        while getopts "s" opt; do
-          case $opt in
-            s) SLEEP=true;;
-            \?) echo "Invalid option. To sleep, use -s";;
-          esac
-        done
+  config =
+    let
+      lockScript =
+        scriptName: cmd:
+        writeScriptBin scriptName ''
+          # Prepare screen
+          pkill polybar || echo "polybar already killed"
+          wk1=$(i3-msg -t get_workspaces | ${jq}/bin/jq '.[] | select(.visible==true).name' | head -1)
+          wk2=$(i3-msg -t get_workspaces | ${jq}/bin/jq '.[] | select(.visible==true).name' | tail -1)
+          i3-msg workspace 11:󰸉
+          i3-msg workspace 12:󰸉
 
-        # Prepare screen
-        pkill polybar || echo "polybar already killed"
-        wk1=$(i3-msg -t get_workspaces | ${jq}/bin/jq '.[] | select(.visible==true).name' | head -1)
-        wk2=$(i3-msg -t get_workspaces | ${jq}/bin/jq '.[] | select(.visible==true).name' | tail -1)
-        i3-msg workspace 11:󰸉
-        i3-msg workspace 12:󰸉
+          ${cmd}
 
-        # Sleep or prepare to sleep
-        if [[ $SLEEP == true ]]; then
-          systemctl suspend
-        else
+          # Lock
+          alock -bg none -cursor blank
+
+          # Restore original config
+          i3-msg workspace "$wk1"
+          i3-msg workspace "$wk2"
+          systemctl --user restart polybar
           pkill xidlehook || echo "xidlehook already killed"
+          xidlehook --timer ${toString (cfg.minutes-before-lock * 60)} 'lock-wait-sleep' ' ' &
+        '';
+      killXidlehook = ''pkill xidlehook || echo "xidlehook already killed"'';
+      lockMode = "Lock: l[o]ck and wait for sleep, lock and [d]on't sleep, [n]ever sleep";
+    in
+    {
+      home.packages = [
+        alock
+        xidlehook
+
+        (lockScript "lock" killXidlehook)
+        (lockScript "lock-wait-sleep" ''
+          ${killXidlehook}
           xidlehook --timer ${toString (cfg.minutes-from-lock-to-sleep * 60)} 'systemctl suspend' ' ' &
-        fi
+        '')
 
-        # Lock
-        alock -bg none -cursor blank
-
-        # Revert to original config
-        i3-msg workspace "$wk1"
-        i3-msg workspace "$wk2"
-        systemctl --user restart polybar
-        pkill xidlehook || echo "xidlehook already killed"
-        xidlehook --timer ${toString (cfg.minutes-before-lock * 60)} 'lock-conky' ' ' &
-      '')
-    ];
-
-    xsession.windowManager.i3.config = {
-      startup = [
-        {
-          command = "xidlehook --timer ${toString (cfg.minutes-before-lock or 3 * 60)} 'lock-conky' ' ' &";
-          notification = false;
-        }
+        (lockScript "lock-sleep" "systemctl suspend")
+        (writeScriptBin "wait-sleep-2-hours" ''
+          ${killXidlehook}
+          xidlehook --timer ${toString (2 * 60 * 60)} 'lock-wait-sleep' ' ' &
+        '')
       ];
-      keybindings = lib.mkOptionDefault {
-        # Lock the screen.
-        "--release ${modifier}+o" = "exec lock-conky";
-      };
-      modes = {
-        ${cfg.i3.exitMode} = {
-          "--release s" = "exec lock-conky -s, mode default";
+
+      xsession.windowManager.i3.config = {
+        startup = [
+          {
+            command = "xidlehook --timer ${
+              toString (cfg.minutes-before-lock or 3 * 60)
+            } 'lock-wait-sleep' ' ' &";
+            notification = false;
+          }
+        ];
+        keybindings = lib.mkOptionDefault {
+          "--release ${modifier}+o" = "mode \"${lockMode}\"";
+        };
+        modes = {
+          ${cfg.i3.exitMode} = {
+            "--release s" = "exec lock-sleep";
+          };
+
+          ${lockMode} = {
+            "--release o" = "exec lock-wait-sleep, mode default";
+            "--release d" = "exec lock, mode default";
+            "--release n" = "exec wait-sleep-2-hours, mode default";
+            Escape = "mode default";
+            Return = "mode default";
+          };
         };
       };
     };
-  };
 
 }
