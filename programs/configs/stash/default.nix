1060746c69b550b120c4ef084b9a20d15cb990cc
RomainC <bebert64@gmail.com>
2025-08-12 17:16:32 +0200

    wip

--- programs/configs/stash/default.nix
+++ programs/configs/stash/default.nix
@@ -25,7 +25,6 @@ in
       activation = {
         symlinkStashConfig = home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
           mkdir -p ${stashDir}/
-
           ln -sf ${nixConfigsRepo}/programs/configs/stash/scrapers ${stashDir}/
         '';
       };
