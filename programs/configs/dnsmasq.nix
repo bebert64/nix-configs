4c3160bbf2904e6c5bb24c887e061d0da01ce783
RomainC <bebert64@gmail.com>
2025-08-20 22:09:33 +0200

    wip

--- programs/configs/dnsmasq.nix
+++ programs/configs/dnsmasq.nix
@@ -4,6 +4,10 @@
     resolveLocalQueries = true;
     settings = {
       address = [ "/.capucina.net/192.168.1.2" ];
+      listen-address = [
+        "127.0.0.1"
+        "192.168.*.*"
+      ];
       domain-needed = true;
       bogus-priv = true;
       filterwin2k = true;
