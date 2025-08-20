de48ca53c55c88da94139205c435735080b669dd
RomainC <bebert64@gmail.com>
2025-08-20 22:17:58 +0200

    wip

--- programs/configs/dnsmasq.nix
+++ programs/configs/dnsmasq.nix
@@ -6,7 +6,7 @@
       address = [ "/.capucina.net/192.168.1.2" ];
       listen-address = [
         "127.0.0.1"
-        "192.168.*.*"
+        "192.168.1.2"
       ];
       domain-needed = true;
       bogus-priv = true;
