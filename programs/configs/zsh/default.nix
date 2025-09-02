1bf5baba25f666109718bff34bfeb901241feddc
RomainC <bebert64@gmail.com>
2025-09-02 16:44:50 +0200

    wip

--- programs/configs/zsh/default.nix
+++ programs/configs/zsh/default.nix
@@ -69,7 +69,8 @@
         update-raspi() {
           cd ~/${cfg.nixConfigsRepo}
           git pull
-          systemd-inhibit nixos-rebuild switch --target-host raspi --build-host localhost --use-remote-sudo --flake .#raspi
+          systemd-inhibit nixos-rebuild build --flake .#raspi
+          systemd-inhibit nixos-rebuild switch --target-host raspi --use-remote-sudo --flake .#raspi
         }
         upgrade() {
           cd ~/${cfg.nixConfigsRepo}
