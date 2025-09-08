c51d93e5914140f9d1a3bdd84455160329a20cd2
RomainC <bebert64@gmail.com>
2025-09-08 10:22:30 +0200

    wip

--- computers/salon/configuration.nix
+++ computers/salon/configuration.nix
@@ -5,6 +5,7 @@
 }:
 let
   user = config.by-db.user;
+  homeDirectory = "/home/${user.name}";
 in
 {
   imports = [
@@ -37,7 +38,62 @@ in
     };
   };
 
-  services.vscode-server.enable = true;
+  services.vscode-server = {
+    enable = true;
+    installPath = [
+      "$HOME/.vscode-server"
+      "$HOME/.vscode-server-oss"
+      "$HOME/.vscode-server-insiders"
+      "$HOME/.cursor-server"
+    ];
+  };
+
+  # systemd.tmpfiles.settings =
+  # let
+  #   createFile = (
+  #     { path, file }:
+  #      {
+  #       # Create the directory so that it has the appropriate permissions if it doesn't already exist
+  #       # Otherwise the directive below creating the symlink would have that owned by root
+  #       name = "${homeDirectory}/${path}";
+  #       value = file user.name;
+  #     }
+  #   );
+  #   # homeDirectory = (
+  #   #   path:
+  #   #   createFile {
+  #   #     inherit path;
+  #   #     file = (
+  #   #       username: {
+  #   #         "d" = {
+  #   #           user = username;
+  #   #           group = "users";
+  #   #           mode = "0755";
+  #   #         };
+  #   #       }
+  #   #     );
+  #   #   }
+  #   # );
+  # in
+  # {
+  #   # We need to create each of the folders before the next file otherwise parents get owned by root
+  #   # "80-setup-config-folder-for-all-users" = homeDirectory ".config";
+  #   # "81-setup-systemd-folder-for-all-users" = homeDirectory ".config/systemd";
+  #   # "82-setup-systemd-user-folder-for-all-users" = homeDirectory ".config/systemd/user";
+  #   "83-enable-auto-fix-vscode-server-service-for-all-users" = createFile {
+  #     path = ".config/systemd/user/auto-fix-vscode-server.service";
+  #     file = (
+  #       username: {
+  #         "L+" = {
+  #           user = username;
+  #           group = "users";
+  #           # This path is made available by `services.vscode-server.enable = true;`
+  #           argument = "/run/current-system/etc/systemd/user/auto-fix-vscode-server.service";
+  #         };
+  #       }
+  #     );
+  #   };
+  # };
 
   networking = {
     hostName = "salon";
