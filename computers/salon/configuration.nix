10741bae4e78f078299f65473cf67b6503b28ea7
RomainC <bebert64@gmail.com>
2025-09-08 09:47:53 +0200

    wip

--- computers/salon/configuration.nix
+++ computers/salon/configuration.nix
@@ -19,17 +19,25 @@ in
     nix-max-ram = "24G";
   };
 
-  home-manager.users.${user.name} = {
-    by-db = {
-      minutes-before-lock = 5;
-      minutes-from-lock-to-sleep = 15;
-      screens = {
-        primary = "HDMI-0";
+  home-manager = {
+    users.${user.name} = {
+      by-db = {
+        minutes-before-lock = 5;
+        minutes-from-lock-to-sleep = 15;
+        screens = {
+          primary = "HDMI-0";
+        };
+        isHeadphonesOnCommand = "pactl get-default-sink | grep alsa_output.pci-0000_00_1b.0.analog-stereo";
+        setHeadphonesCommand = "set-default-sink alsa_output.pci-0000_00_1b.0.analog-stereo";
+        setSpeakerCommand = "set-default-sink alsa_output.pci-0000_01_00.1.hdmi-stereo-extra2";
       };
-      isHeadphonesOnCommand = "pactl get-default-sink | grep alsa_output.pci-0000_00_1b.0.analog-stereo";
-      setHeadphonesCommand = "set-default-sink alsa_output.pci-0000_00_1b.0.analog-stereo";
-      setSpeakerCommand = "set-default-sink alsa_output.pci-0000_01_00.1.hdmi-stereo-extra2";
     };
+
+    imports = [
+      "${fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master"}/modules/vscode-server/home.nix"
+    ];
+
+    services.vscode-server.enable = true;
   };
 
   networking = {
