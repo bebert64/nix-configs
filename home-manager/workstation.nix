6c1385d9f1b923d3c5d4d980777e300ffe8bab78
RomainC <bebert64@gmail.com>
2025-09-21 23:09:30 +0200

    wip

--- home-manager/workstation.nix
+++ home-manager/workstation.nix
@@ -9,7 +9,6 @@
 let
   inherit (lib) mkEnableOption mkOption types;
   inherit (types) str;
-  cfg = config.by-db;
 in
 {
   imports = [
@@ -18,7 +17,7 @@ in
     ../fonts
     by-db.module.x86_64-linux
   ]
-  ++ lib.optionals cfg.generativeAi.enable [ nix-ai.homeManagerModules.default ];
+  ++ lib.optionals config.by-db.generativeAi.enable [ nix-ai.homeManagerModules.default ];
 
   options.by-db = {
     bluetooth.enable = mkEnableOption "Whether to activate or not the blueman applet";
@@ -45,89 +44,92 @@ in
     wifi.enable = mkEnableOption "Whether or not to install network manager";
   };
 
-  config = {
-    home = {
+  config =
+    let
+      cfg = config.by-db;
+    in
+    {
+      home = {
+        packages =
+          (with pkgs; [
+            anydesk
+            arandr # GUI to configure screens positions (need to kill autorandr)
+            chromium
+            evince # pdf reader
+            filezilla
+            fusee-launcher
+            gnome-keyring
+            inkscape
+            microcodeIntel # for increased microprocessor performance
+            mcomix
+            pavucontrol # pulse audio volume controle
+            vlc
+          ])
+          ++ lib.optionals cfg.wifi.enable (
+            with pkgs;
+            [
+              networkmanager
+              networkmanagerapplet
+            ]
+          );
+      };
 
-      packages =
-        (with pkgs; [
-          anydesk
-          arandr # GUI to configure screens positions (need to kill autorandr)
-          chromium
-          evince # pdf reader
-          filezilla
-          fusee-launcher
-          gnome-keyring
-          inkscape
-          microcodeIntel # for increased microprocessor performance
-          mcomix
-          pavucontrol # pulse audio volume controle
-          vlc
-        ])
-        ++ lib.optionals cfg.wifi.enable (
-          with pkgs;
-          [
-            networkmanager
-            networkmanagerapplet
-          ]
-        );
-    };
+      programs = {
+        home-manager.enable = true;
+      };
 
-    programs = {
-      home-manager.enable = true;
-    };
+      services = {
+        playerctld.enable = true;
+        caffeine.enable = true;
+      };
 
-    services = {
-      playerctld.enable = true;
-      caffeine.enable = true;
-    };
+      by-db-pkgs = {
+        compress-videos = {
+          enable = true;
+          jellyfin.accessToken = "${config.sops.secrets."jellyfin/access-token".path}";
+          stash.apiKey = "${config.sops.secrets."stash/api-key".path}";
+        };
+        guitar-tutorials = {
+          app.enable = true;
+          firefox.ffsync = cfg.ffsync.bebert64;
+          jellyfin.accessToken = "${config.sops.secrets."jellyfin/access-token".path}";
+        };
+        shortcuts = {
+          app.enable = true;
+          postgres = cfg.postgres;
+          firefox.ffsync = cfg.ffsync.shortcutsDb;
+          stashApiConfig.apiKey = "${config.sops.secrets."stash/api-key".path}";
+        };
+        wallpapers-manager = {
+          services = {
+            change = {
+              enable = true;
+              commandArgs = "--mode fifty-fifty";
+            };
+          };
+          firefox.ffsync = cfg.ffsync.bebert64;
+        };
+      };
 
-    by-db-pkgs = {
-      compress-videos = {
+      xdg = {
         enable = true;
-        jellyfin.accessToken = "${config.sops.secrets."jellyfin/access-token".path}";
-        stash.apiKey = "${config.sops.secrets."stash/api-key".path}";
-      };
-      guitar-tutorials = {
-        app.enable = true;
-        firefox.ffsync = cfg.ffsync.bebert64;
-        jellyfin.accessToken = "${config.sops.secrets."jellyfin/access-token".path}";
-      };
-      shortcuts = {
-        app.enable = true;
-        postgres = cfg.postgres;
-        firefox.ffsync = cfg.ffsync.shortcutsDb;
-        stashApiConfig.apiKey = "${config.sops.secrets."stash/api-key".path}";
-      };
-      wallpapers-manager = {
-        services = {
-          change = {
-            enable = true;
-            commandArgs = "--mode fifty-fifty";
+        mimeApps = {
+          enable = true;
+          associations.added = {
+            "defaut-web-browser" = [ "firefox.desktop" ];
+            "text/html" = [ "firefox.desktop" ];
+            "text/xml" = [ "firefox.desktop" ];
+            "x-scheme-handler/http" = [ "firefox.desktop" ];
+            "x-scheme-handler/https" = [ "firefox.desktop" ];
+          };
+          defaultApplications = {
+            "defaut-web-browser" = [ "firefox.desktop" ];
+            "text/html" = [ "firefox.desktop" ];
+            "text/xml" = [ "firefox.desktop" ];
+            "x-scheme-handler/http" = [ "firefox.desktop" ];
+            "x-scheme-handler/https" = [ "firefox.desktop" ];
           };
         };
-        firefox.ffsync = cfg.ffsync.bebert64;
       };
     };
-
-    xdg = {
-      enable = true;
-      mimeApps = {
-        enable = true;
-        associations.added = {
-          "defaut-web-browser" = [ "firefox.desktop" ];
-          "text/html" = [ "firefox.desktop" ];
-          "text/xml" = [ "firefox.desktop" ];
-          "x-scheme-handler/http" = [ "firefox.desktop" ];
-          "x-scheme-handler/https" = [ "firefox.desktop" ];
-        };
-        defaultApplications = {
-          "defaut-web-browser" = [ "firefox.desktop" ];
-          "text/html" = [ "firefox.desktop" ];
-          "text/xml" = [ "firefox.desktop" ];
-          "x-scheme-handler/http" = [ "firefox.desktop" ];
-          "x-scheme-handler/https" = [ "firefox.desktop" ];
-        };
-      };
-    };
-  };
 }
