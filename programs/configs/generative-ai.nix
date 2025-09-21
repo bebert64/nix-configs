7f9c35103631524ae5d3c4111b79f736f0218608
RomainC <bebert64@gmail.com>
2025-09-21 23:05:36 +0200

    wip

--- programs/configs/generative-ai.nix
+++ programs/configs/generative-ai.nix
@@ -1,17 +1,18 @@
 {
-  pkgs,
   config,
-  nix-ai,
   lib,
   ...
 }:
 {
   config = lib.mkIf config.by-db.generativeAi.enable {
-    home.packages = [ nix-ai.packages.${pkgs.system}.default ];
     services = {
       ollama = {
         enable = true;
         acceleration = "cuda";
+      };
+      nixai = {
+        enable = true;
+        mcp.enable = true;
       };
     };
   };
