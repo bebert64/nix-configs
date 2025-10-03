b920e3a65bded47913627a338bc8617f53b54f35
RomainC <bebert64@gmail.com>
2025-10-03 15:30:30 +0200

    wip

--- programs/nginx/default.nix
+++ programs/nginx/default.nix
@@ -46,6 +46,22 @@
           "/" = {
             proxyPass = "http://192.168.1.6:8188";
             proxyWebsockets = true;
+            extraConfig = ''
+              proxy_http_version 1.1;
+
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection "upgrade";
+
+              proxy_set_header Host $host;
+              proxy_set_header Origin $scheme://$host;
+
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+
+              proxy_read_timeout 86400;
+            '';
+
           };
         };
       };
