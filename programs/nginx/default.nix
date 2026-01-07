ae39173fe4ee593b38ffed57845e07585b23f0b6
RomainC <bebert64@gmail.com>
2026-01-07 21:49:13 +0100

    wip

--- programs/nginx/default.nix
+++ programs/nginx/default.nix
@@ -122,20 +122,39 @@ in
       "torrent.capucina.net" = {
         enableACME = true;
         forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://192.168.1.7:8080";
-          extraConfig = ''
-            proxy_http_version 1.1;
-            # headers recognized by qBittorrent
-            proxy_set_header   Host               $proxy_host;
-            proxy_set_header   X-Forwarded-For    $proxy_add_x_forwarded_for;
-            proxy_set_header   X-Forwarded-Host   $http_host;
-            proxy_set_header   X-Forwarded-Proto  $scheme;
-          '';
+
+        # Serve the "down" page from a static path
+        root = "/var/www/capucina.net/qbittorrent"; # directory for down.html
+
+        locations = {
+          "/" = {
+            proxyPass = "http://192.168.1.7:8080";
+            extraConfig = ''
+              proxy_http_version 1.1;
+              # headers recognized by qBittorrent
+              proxy_set_header   Host               $proxy_host;
+              proxy_set_header   X-Forwarded-For    $proxy_add_x_forwarded_for;
+              proxy_set_header   X-Forwarded-Host   $http_host;
+              proxy_set_header   X-Forwarded-Proto  $scheme;
+
+              # Detect if backend is down and serve down.html
+              proxy_connect_timeout 1s;
+              proxy_read_timeout 3s;
+              proxy_send_timeout 3s;
+              proxy_intercept_errors on;
+              error_page 502 503 504 /down.html;
+            '';
+          };
+          # Serve static down.html page
+          "/down.html" = {
+            root = "/var/www/capucina.net/qbittorrent";
+          };
         };
       };
     };
   };
+
+  file."/var/www/capucina.net/qbittorrent/down.html".source = ./qbittorrent-down.html;
 
   security.acme = {
     acceptTerms = true;
