686b19a2c080f1e6861fa78e842fa1c202a3f536
RomainC <bebert64@gmail.com>
2026-01-07 20:40:55 +0100

    wip

--- nixos/common.nix
+++ nixos/common.nix
@@ -139,7 +139,7 @@
         };
       };
 
-      # Necessary for remote installation, using --use-remote-sudo or to get access to additional caches
+      # Necessary for remote installation, using --sudo or to get access to additional caches
       nix.settings.trusted-users = [ "${cfg.user.name}" ];
 
       # This value determines the NixOS release from which the default
