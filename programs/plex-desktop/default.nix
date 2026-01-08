4ed6fb6eafa98848ab0e751a6216ee93ece349fd
RomainC <bebert64@gmail.com>
2026-01-08 15:43:51 +0100

    wip

--- programs/plex-desktop/default.nix
+++ programs/plex-desktop/default.nix
@@ -8,13 +8,13 @@ let
   modifier = config.xsession.windowManager.i3.config.modifier;
 in
 {
-  home.packages = [ pkgs.plex-media-player ];
+  home.packages = [ pkgs.plex-desktop ];
   xsession.windowManager.i3.config = {
     assigns = {
       "$ws8" = [ { class = "Plex"; } ];
     };
     keybindings = lib.mkOptionDefault {
-      "${modifier}+Control+p" = "workspace $ws8; exec plex-media-player";
+      "${modifier}+Control+p" = "workspace $ws8; exec plex-desktop";
     };
   };
 }
