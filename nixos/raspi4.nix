b115699f34e4b9bd23ab6850c91e4f8a3076912f
RomainC <bebert64@gmail.com>
2026-01-13 17:28:27 +0100

    wip

--- nixos/raspi4.nix
+++ nixos/raspi4.nix
@@ -18,26 +18,43 @@
     {
       home-manager.users.${cfg.user.name}.imports = [ ../home-manager/raspi4.nix ];
 
-      networking.firewall = {
-        enable = true;
-        allowedTCPPorts = [
-          80 # http
-          443 # https
-          5432 # PostgreSQL forwarding
-        ];
-      };
+      networking = {
+        firewall = {
+          enable = true;
+          allowedTCPPorts = [
+            80 # http
+            443 # https
+            5432 # PostgreSQL forwarding
+            51820 # VPN
+          ];
+        };
 
-      # Forward port 5432 to 192.168.1.7:5432
-      networking.nat = {
-        enable = true;
-        externalInterface = "eth0";
-        forwardPorts = [
-          {
-            sourcePort = 5432;
-            destination = "192.168.1.7:5432";
-            proto = "tcp";
-          }
-        ];
+        # Forward port 5432 to 192.168.1.7:5432
+        nat = {
+          enable = true;
+          externalInterface = "eth0";
+          forwardPorts = [
+            {
+              sourcePort = 5432;
+              destination = "192.168.1.7:5432";
+              proto = "tcp";
+            }
+          ];
+        };
+
+        # VPN server
+        wireguard = {
+          enable = true;
+          interfaces = {
+            wg0 = {
+              address = [ "10.200.200.1/24" ]; # VPN subnet
+              listenPort = 51820; # UDP port for VPN
+              privateKeyFile = "/etc/wireguard/privatekey";
+              peers = [ ]; # We'll add clients later
+            };
+          };
+        };
+
       };
     };
 }
