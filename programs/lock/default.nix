d84cc0fd49bfdaffa3d33b4cc10002e9bb665948
RomainC <bebert64@gmail.com>
2026-02-19 10:55:46 +0100

    Don't go to sleep while incoming ssh sessions are active

--- programs/lock/default.nix
+++ programs/lock/default.nix
@@ -33,6 +33,21 @@ in
 
   config =
     let
+      # Auto-sleep: wait for incoming SSH connections to disconnect (retry every 2 min), then suspend.
+      # Use /proc/net/tcp to avoid ss (which can be aliased to systemctl status). Port 22 = 0x0016 hex.
+      suspendIfNoIncomingSsh = writeScriptBin "suspend-if-no-incoming-ssh" ''
+        #!${pkgs.runtimeShell}
+        set -e
+        retry_interval=120
+        has_incoming_ssh() {
+          [ -n "$(awk 'FNR>1 && $4=="01" && $2~/:0016$/' /proc/net/tcp /proc/net/tcp6 2>/dev/null)" ]
+        }
+        while has_incoming_ssh; do
+          sleep "$retry_interval"
+        done
+        exec systemctl suspend
+      '';
+
       lockScript =
         scriptName: cmd:
         writeScriptBin scriptName ''
@@ -65,13 +80,17 @@ in
         xidlehook
         (lockScript "lock" ''
           ${killXidlehook}
-          xidlehook --timer ${toString (cfg.minutes-from-lock-to-sleep * 60)} 'systemctl suspend' ' ' &
+          xidlehook --timer ${
+            toString (cfg.minutes-from-lock-to-sleep * 60)
+          } 'suspend-if-no-incoming-ssh' ' ' &
         '')
+        # Manual sleep (press s): always suspend immediately, ignore SSH
         (lockScript "lock-sleep" "sleep 1 && systemctl suspend")
         (lockScript "lock-dont-sleep" ''
           ${killXidlehook}
           xidlehook --timer ${toString (cfg.minutes-from-lock-to-sleep * 60)} 'xset dpms force off' ' ' &
         '')
+        suspendIfNoIncomingSsh
       ];
 
       xsession.windowManager.i3.config = {
