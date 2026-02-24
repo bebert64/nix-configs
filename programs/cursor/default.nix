e2620dcfcecdedf25b3e984347a5cff9331f38c8
RomainC <bebert64@gmail.com>
2026-02-24 16:21:16 +0100

    wip

--- programs/cursor/default.nix
+++ programs/cursor/default.nix
@@ -127,6 +127,7 @@ in
       "${modifier}+Shift+v" = "workspace $ws3; exec ${openCerberus}";
       "${modifier}+Mod1+v" = "workspace $ws3; exec ${openSalon}";
       "${modifier}+Control+n" = "workspace $ws3; exec ${openNixLocal}";
+      "${modifier}+Shift+n" = "workspace $ws3; exec cursor --folder-uri=vscode-remote://ssh-remote+cerberus/home/romain/nix-configs";
       "${modifier}+Mod1+n" = "workspace $ws3; exec ${openNixSalon}";
     };
   };
