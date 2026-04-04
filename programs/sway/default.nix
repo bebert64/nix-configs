25a9d4b2d441c75a96cd25354702704d5d3d267c
RomainC <bebert64@gmail.com>
2026-04-04 21:48:29 +0200

    wip

--- programs/sway/default.nix
+++ programs/sway/default.nix
@@ -9,6 +9,28 @@ let
   exitMode = homeManagerBydbConfig.sway.exitMode;
   jq = "${pkgs.jq}/bin/jq";
   swaymsg = "${pkgs.sway}/bin/swaymsg";
+  ws = {
+    "1" = "1:";
+    "2" = "2:";
+    "3" = "3:";
+    "4" = "4:";
+    "5" = "5:";
+    "6" = "6:";
+    "7" = "7:";
+    "8" = "8:";
+    "9" = "9:󱓞";
+    "10" = "10:";
+    "11" = "11:󰷝";
+    "12" = "12:";
+    "13" = "13:󰖔";
+    "14" = "14";
+    "15" = "15";
+    "16" = "16";
+    "17" = "17";
+    "18" = "18";
+    "19" = "19:󰸉";
+    "20" = "20:󰸉";
+  };
   moveWorkspaceToOutput =
     direction:
     assert direction == "left" || direction == "right";
@@ -67,61 +89,61 @@ in
 
         keybindings = lib.mkOptionDefault {
           # Workspace definition (AZERTY: unshifted keys are &éà"'(-è_ç)
-          "${modifier}+ampersand" = "workspace number 1";
-          "${modifier}+eacute" = "workspace number 2";
-          "${modifier}+quotedbl" = "workspace number 3";
-          "${modifier}+apostrophe" = "workspace number 4";
-          "${modifier}+parenleft" = "workspace number 5";
-          "${modifier}+minus" = "workspace number 6";
-          "${modifier}+egrave" = "workspace number 7";
-          "${modifier}+underscore" = "workspace number 8";
-          "${modifier}+ccedilla" = "workspace number 9";
-          "${modifier}+agrave" = "workspace number 10";
-          "${modifier}+Ctrl+ampersand" = "workspace number 11";
-          "${modifier}+Ctrl+eacute" = "workspace number 12";
-          "${modifier}+Ctrl+quotedbl" = "workspace number 13";
-          "${modifier}+Ctrl+apostrophe" = "workspace number 14";
-          "${modifier}+Ctrl+parenleft" = "workspace number 15";
-          "${modifier}+Ctrl+minus" = "workspace number 16";
-          "${modifier}+Ctrl+egrave" = "workspace number 17";
-          "${modifier}+Ctrl+underscore" = "workspace number 18";
-          "${modifier}+Ctrl+ccedilla" = "workspace number 19";
-          "${modifier}+Ctrl+agrave" = "workspace number 20";
+          "${modifier}+ampersand" = "workspace \"${ws."1"}\"";
+          "${modifier}+eacute" = "workspace \"${ws."2"}\"";
+          "${modifier}+quotedbl" = "workspace \"${ws."3"}\"";
+          "${modifier}+apostrophe" = "workspace \"${ws."4"}\"";
+          "${modifier}+parenleft" = "workspace \"${ws."5"}\"";
+          "${modifier}+minus" = "workspace \"${ws."6"}\"";
+          "${modifier}+egrave" = "workspace \"${ws."7"}\"";
+          "${modifier}+underscore" = "workspace \"${ws."8"}\"";
+          "${modifier}+ccedilla" = "workspace \"${ws."9"}\"";
+          "${modifier}+agrave" = "workspace \"${ws."10"}\"";
+          "${modifier}+Ctrl+ampersand" = "workspace \"${ws."11"}\"";
+          "${modifier}+Ctrl+eacute" = "workspace \"${ws."12"}\"";
+          "${modifier}+Ctrl+quotedbl" = "workspace \"${ws."13"}\"";
+          "${modifier}+Ctrl+apostrophe" = "workspace \"${ws."14"}\"";
+          "${modifier}+Ctrl+parenleft" = "workspace \"${ws."15"}\"";
+          "${modifier}+Ctrl+minus" = "workspace \"${ws."16"}\"";
+          "${modifier}+Ctrl+egrave" = "workspace \"${ws."17"}\"";
+          "${modifier}+Ctrl+underscore" = "workspace \"${ws."18"}\"";
+          "${modifier}+Ctrl+ccedilla" = "workspace \"${ws."19"}\"";
+          "${modifier}+Ctrl+agrave" = "workspace \"${ws."20"}\"";
 
           # Workspace move container to
-          "${modifier}+Shift+ampersand" = "move container to workspace number 1";
-          "${modifier}+Shift+eacute" = "move container to workspace number 2";
-          "${modifier}+Shift+quotedbl" = "move container to workspace number 3";
-          "${modifier}+Shift+apostrophe" = "move container to workspace number 4";
-          "${modifier}+Shift+parenleft" = "move container to workspace number 5";
-          "${modifier}+Shift+minus" = "move container to workspace number 6";
-          "${modifier}+Shift+egrave" = "move container to workspace number 7";
-          "${modifier}+Shift+underscore" = "move container to workspace number 8";
-          "${modifier}+Shift+ccedilla" = "move container to workspace number 9";
-          "${modifier}+Shift+agrave" = "move container to workspace number 10";
-          "${modifier}+Shift+Ctrl+ampersand" = "move container to workspace number 11";
-          "${modifier}+Shift+Ctrl+eacute" = "move container to workspace number 12";
-          "${modifier}+Shift+Ctrl+quotedbl" = "move container to workspace number 13";
-          "${modifier}+Shift+Ctrl+apostrophe" = "move container to workspace number 14";
-          "${modifier}+Shift+Ctrl+parenleft" = "move container to workspace number 15";
-          "${modifier}+Shift+Ctrl+minus" = "move container to workspace number 16";
-          "${modifier}+Shift+Ctrl+egrave" = "move container to workspace number 17";
-          "${modifier}+Shift+Ctrl+underscore" = "move container to workspace number 18";
-          "${modifier}+Shift+Ctrl+ccedilla" = "move container to workspace number 19";
-          "${modifier}+Shift+Ctrl+agrave" = "move container to workspace number 20";
+          "${modifier}+Shift+ampersand" = "move container to workspace \"${ws."1"}\"";
+          "${modifier}+Shift+eacute" = "move container to workspace \"${ws."2"}\"";
+          "${modifier}+Shift+quotedbl" = "move container to workspace \"${ws."3"}\"";
+          "${modifier}+Shift+apostrophe" = "move container to workspace \"${ws."4"}\"";
+          "${modifier}+Shift+parenleft" = "move container to workspace \"${ws."5"}\"";
+          "${modifier}+Shift+minus" = "move container to workspace \"${ws."6"}\"";
+          "${modifier}+Shift+egrave" = "move container to workspace \"${ws."7"}\"";
+          "${modifier}+Shift+underscore" = "move container to workspace \"${ws."8"}\"";
+          "${modifier}+Shift+ccedilla" = "move container to workspace \"${ws."9"}\"";
+          "${modifier}+Shift+agrave" = "move container to workspace \"${ws."10"}\"";
+          "${modifier}+Shift+Ctrl+ampersand" = "move container to workspace \"${ws."11"}\"";
+          "${modifier}+Shift+Ctrl+eacute" = "move container to workspace \"${ws."12"}\"";
+          "${modifier}+Shift+Ctrl+quotedbl" = "move container to workspace \"${ws."13"}\"";
+          "${modifier}+Shift+Ctrl+apostrophe" = "move container to workspace \"${ws."14"}\"";
+          "${modifier}+Shift+Ctrl+parenleft" = "move container to workspace \"${ws."15"}\"";
+          "${modifier}+Shift+Ctrl+minus" = "move container to workspace \"${ws."16"}\"";
+          "${modifier}+Shift+Ctrl+egrave" = "move container to workspace \"${ws."17"}\"";
+          "${modifier}+Shift+Ctrl+underscore" = "move container to workspace \"${ws."18"}\"";
+          "${modifier}+Shift+Ctrl+ccedilla" = "move container to workspace \"${ws."19"}\"";
+          "${modifier}+Shift+Ctrl+agrave" = "move container to workspace \"${ws."20"}\"";
 
           "${modifier}+Mod1+Left" = "exec ${moveWorkspaceToOutput "left"}";
           "${modifier}+Mod1+Right" = "exec ${moveWorkspaceToOutput "right"}";
 
           # Used to display empty workspaces, allowing to see the wallpapers
-          "${modifier}+i" = "workspace number 19; workspace number 20";
+          "${modifier}+i" = "workspace \"${ws."19"}\"; workspace \"${ws."20"}\"";
 
           # Modes
           "${modifier}+Shift+e" = "mode \"${exitMode}\"";
         };
 
         startup = [
-          { command = "swaymsg workspace number 1"; }
+          { command = "swaymsg workspace \"${ws."1"}\""; }
           # https://wiki.archlinux.org/title/GNOME/Keyring#Launching_gnome-keyring-daemon_outside_desktop_environments_(KDE,_GNOME,_XFCE,_...)
           { command = "dbus-update-activation-environment --all; gnome-keyring-daemon --start --components=secrets"; }
           { command = "swww-daemon"; }
@@ -161,29 +183,9 @@ in
       };
 
       extraConfig = ''
-        set $ws1 "1:"
-        set $ws2 "2:"
-        set $ws3 "3:"
-        set $ws4 "4:"
-        set $ws5 "5:"
-        set $ws6 "6:"
-        set $ws7 "7:"
-        set $ws8 "8:"
-        set $ws9 "9:󱓞"
-        set $ws10 "10:"
-        workspace $ws10 gaps inner 80
-        set $ws11 "11:󰷝"
-        set $ws12 "12:"
-        set $ws13 "13:󰖔"
-        set $ws14 "14"
-        set $ws15 "15"
-        set $ws16 "16"
-        set $ws17 "17"
-        set $ws18 "18"
-        set $ws19 "19:󰸉"
-        workspace $ws19 output ${homeManagerBydbConfig.screens.primary}
-        set $ws20 "20:󰸉"
-        workspace $ws20 output ${homeManagerBydbConfig.screens.secondary}
+        workspace "${ws."10"}" gaps inner 80
+        workspace "${ws."19"}" output ${homeManagerBydbConfig.screens.primary}
+        workspace "${ws."20"}" output ${homeManagerBydbConfig.screens.secondary}
 
         blur enable
         blur_passes 2
