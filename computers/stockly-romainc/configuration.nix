93531816d7ccd228127931167a723a280d8932dc
RomainC <bebert64@gmail.com>
2026-04-06 10:48:10 +0200

    Use custom path for nix-configs repo on stockly-romainc

--- computers/stockly-romainc/configuration.nix
+++ computers/stockly-romainc/configuration.nix
@@ -26,7 +26,6 @@ in
       byDb = {
         batteryNotifier.enable = true;
         wifi.enable = true;
-        nixConfigsRelativePath = "nix-config";
         isHeadphonesOnCommand = "pactl info | grep \"Default Sink.*Headphones\"";
         setHeadphonesCommand = "set-card-profile 'alsa_card.pci-0000_00_1f.3-platform-skl_hda_dsp_generic' 'HiFi (HDMI1, HDMI2, HDMI3, Headphones, Mic1, Mic2)'";
         setSpeakerCommand = "set-card-profile 'alsa_card.pci-0000_00_1f.3-platform-skl_hda_dsp_generic' 'HiFi (HDMI1, HDMI2, HDMI3, Mic1, Mic2, Speaker)'";
@@ -49,6 +48,7 @@ in
     ui = null;
     nvidia = false;
     auto-update.enable = true; # See auto-update.nix for doc
+    auto-update.flakePath = "/home/user/code/nix-configs";
     keyboard-layout = "fr";
   };
 }
