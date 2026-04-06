fcfb66425729cc71b39fb644a952955a1b4e1935
RomainC <romain@stockly.ai>
2026-04-06 10:25:58 +0200

    Fix: deprecated warning when installing vdhcoapp

--- programs/vdhcoapp/default.nix
+++ programs/vdhcoapp/default.nix
@@ -4,7 +4,7 @@
   home = {
     activation = {
       installVdhcoapp = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        ${pkgs.vdhcoapp}/bin/vdhcoapp install
+        ${pkgs.vdhcoapp}/bin/vdhcoapp add
       '';
     };
   };
